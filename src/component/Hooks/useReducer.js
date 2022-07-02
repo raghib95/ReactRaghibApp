348462f487243902c9867be9063fe944b2cd8fad
Raghib Alam <raghibalam1995@gmail.com>
2022-07-02 20:46:19 +0530

    N

---
+++ src/component/Hooks/useReducer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useReducer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useReducer
---
+++ src/component/Hooks/useState.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useState
